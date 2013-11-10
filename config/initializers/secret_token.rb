599ae17111d9c6e5ca248288808b8a2dffdf536f
Mark Lee <mlbgit>
2013-11-10 07:39:54 +0000

    initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+DemoApp::Application.config.secret_key_base = '3951660ecd968b080ada8406f501491db053a2f9461fb9eaa8526a668ef88eb8e1a5867edcf54f4bb7749b6a00daba978f28ceb0fee0eb2819eda6b885a9a280'
